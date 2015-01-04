8bd63f48b4a0732d04fef2207316c2709e197051
AsavarTzeth <asavartzeth@gmail.com>
2015-01-04 22:27:29 +0100

    Readability cleanup
    - Added very newcomer friendly comments to the bootstrap.sh script.
    - Replaced whitespaces with tab based indentation. I recommend using editor
      settings instead, if you wish to keep it 2 spaces long.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,56 @@
 #!/bin/sh
+
 set -e
-if [ -z "$1" ]
-then
-  echo "Usage: $0 username" >&2
-  exit 1
+
+# Check if script is run correctly
+if [ -z "$1" ]; then
+	echo "Usage: $0 username" >&2
+	exit 1
 fi
+
 username="$1"
+
+# Download/Update all dependencies
 git submodule update --init
+
+# Clean/Remove built environment
 docker rmi mwcampbell/muslbase-build-base || true
 docker rmi $username/muslbase-build || true
-for buildbase in debian selfhost selfhost
-do
-  if [ "$buildbase" = "selfhost" ]
-  then
-    docker tag $username/muslbase mwcampbell/muslbase-build-base
-  else
-    docker build --rm -t=mwcampbell/muslbase-build-base buildbase/$buildbase
-  fi
-  docker rmi $username/muslbase || true
-  docker rmi $username/muslbase-runtime || true
-  docker rmi $username/muslbase-static-runtime || true
-  docker build --rm -t=$username/muslbase-build .
-  docker run --rm $username/muslbase-build cat /rootfs.tar > rootfs/full/rootfs.tar
-  docker build --rm -t=$username/muslbase rootfs/full
-  rm rootfs/full/rootfs.tar
-  docker run --rm $username/muslbase-build cat /runtime-rootfs.tar > rootfs/runtime/rootfs.tar
-  docker build --rm -t=$username/muslbase-runtime rootfs/runtime
-  rm rootfs/runtime/rootfs.tar
-  docker run --rm $username/muslbase-build cat /static-runtime-rootfs.tar > rootfs/static-runtime/rootfs.tar
-  docker build --rm -t=$username/muslbase-static-runtime rootfs/static-runtime
-  rm rootfs/static-runtime/rootfs.tar
-  docker rmi mwcampbell/muslbase-build-base
-  docker rmi $username/muslbase-build
+
+# Build, first using glibc from debian, then musl from selfhost build environment
+for buildbase in debian selfhost selfhost; do
+	if [ "$buildbase" = "selfhost" ]; then
+		# Next use muslbase as build environment
+		docker tag $username/muslbase mwcampbell/muslbase-build-base
+	else
+		# Build initial debian base build environment
+		docker build --rm -t=mwcampbell/muslbase-build-base buildbase/$buildbase
+	fi
+
+	# Remove earlier muslbase images
+	docker rmi $username/muslbase || true
+	docker rmi $username/muslbase-runtime || true
+	docker rmi $username/muslbase-static-runtime || true
+
+	# Build muslbase rootfs
+	docker build --rm -t=$username/muslbase-build .
+
+	# Build docker image: muslbase
+	docker run --rm $username/muslbase-build cat /rootfs.tar > rootfs/full/rootfs.tar
+	docker build --rm -t=$username/muslbase rootfs/full
+	rm rootfs/full/rootfs.tar
+
+	# Build docker image: muslbase-runtime
+	docker run --rm $username/muslbase-build cat /runtime-rootfs.tar > rootfs/runtime/rootfs.tar
+	docker build --rm -t=$username/muslbase-runtime rootfs/runtime
+	rm rootfs/runtime/rootfs.tar
+
+	# Build docker image: muslbase-static-runtime
+	docker run --rm $username/muslbase-build cat /static-runtime-rootfs.tar > rootfs/static-runtime/rootfs.tar
+	docker build --rm -t=$username/muslbase-static-runtime rootfs/static-runtime
+	rm rootfs/static-runtime/rootfs.tar
+
+	# Clean/Remove build environment
+	docker rmi mwcampbell/muslbase-build-base
+	docker rmi $username/muslbase-build
 done
